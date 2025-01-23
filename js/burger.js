de7a269f4c2d517804489e6e0dc9f9b4c618c438
Zubko1994 <zubko721994@gmail.com>
2025-01-23 19:09:56 +0300

    last change 23.01

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('.burger__menu')
 const menu = document.querySelector('.header__nav')
 burger.addEventListener('click',() => {
+    document.body.style.overflow = "hidden"
+    if(burger.classList.contains("burger-active")) document.body.style.overflow = "auto"
     burger.classList.toggle('burger-active')
     menu.classList.toggle('header__nav-active')
 })
